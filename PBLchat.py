4fc11bf9cd8655368d9478dfb2b44a444067087b
Mayuresh123 <56029669+mayuresh714@users.noreply.github.com>
2021-02-23 10:19:45 +0530

    Update PBLchat.py

--- PBLchat.py
+++ PBLchat.py
@@ -62,7 +62,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
-
+ 
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    msg = request.form.get('Body').lower()
+    am = msg
+    remote_number = request.values.get("From", "").split(":")[1]    
+    return trans_msg(am,remote_number)
 
 if __name__ == "__main__":
     app.run(debug=True)
